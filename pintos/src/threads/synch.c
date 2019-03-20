425911ac1496c7eee3d7727be9996e46e8f84020
Park Kee Hun <qkrrlgns743@unist.ac.kr>
2019-03-20 20:16:25 +0900

    Pass the test of priority-sema

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -68,7 +68,11 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      //list_push_back (&sema->waiters, &thread_current ()->elem);
+      //Arrange waiters list in sema in ordered with priority.
+      //waiters list's front has highest priority.
+      list_insert_ordered (&sema->waiters, &thread_current ()->elem,
+			   prior_thread, NULL);
       thread_block ();
     }
   sema->value--;
@@ -113,11 +117,16 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
+  if (!list_empty (&sema->waiters))
+  { 
+    list_sort (&sema->waiters, prior_thread, NULL);
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
+  }
   sema->value++;
   intr_set_level (old_level);
+
+  thread_yield ();
 }
 
 static void sema_test_helper (void *sema_);
